8a52b614cdaa2b9583b7209e80d0b3bfabff0331
franlr <franlr@users.noreply.github.com>
2014-12-01 11:29:39 +0100

    Create ansible_install_on_debian_wheezy.sh

---
+++ ansible_install_on_debian_wheezy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Ansible on Debian Wheezy
+
+# Requirements
+apt-get install -y git-core
+apt-get install -y build-essential git-core libssl-dev curl wget
+
+# facter (2.3.0)
+apt-get install -y lsb-release
+wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+dpkg -i puppetlabs-release-wheezy.deb
+apt-get update
+apt-get install -y facter=2.3.0-1puppetlabs1_all.deb
+
+# Install from repositories (1.7.2)
+#apt-get install -y python-setuptools python-dev python-yaml python-paramiko python-jinja2 sshpass
+#apt-get install -y python-pip
+#apt-get install -y -t wheezy-backports ansible
+
+# Install from source (1.8.0)
+git clone --branch v1.8.0 https://github.com/ansible/ansible.git
+apt-get install -y python-setuptools python-pip python-dev sshpass
+pip install PyYAML jinja2 paramiko
+cd ansible
+make install
+
+# Configuration
+sh -c 'echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config'
