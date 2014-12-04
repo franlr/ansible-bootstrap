e4ef98dad34d3e6f095b2fdbac1749049ad68d3d
franlr <franlr@users.noreply.github.com>
2014-12-04 11:54:48 +0100

    Update ansible_install_on_centos_6.sh

--- ansible_install_on_centos_6.sh
+++ ansible_install_on_centos_6.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 # Requires epel repo enabled
-yum --enablerepo=epel* install -y ansible sshpass facter gmp python-pip
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+#
+# From centos repositories or from yum.puppetlabs.com:
+yum install facter
+
+yum install -y python-devel python-setuptools
+yum --enablerepo=epel* install -y ansible sshpass gmp python-pip
 # or installed from source
-yum installl -y "Development tools"
+yum groupinstall -y 'Development Tools'
 cd /opt
 git clone git://github.com/ansible/ansible.git
 cd ./ansible
 make rpm
 rpm -Uvh ~/rpmbuild/ansible-*.noarch.rpm
+
+export ANSIBLE_HOSTS=/etc/ansible/hosts
