fc35a0cec4507bb7143b69e06585fa0c5b50caba
franlr <franlr@users.noreply.github.com>
2014-12-23 09:51:42 +0100

    Update ansible_install_on_debian_wheezy.sh

--- ansible_install_on_debian_wheezy.sh
+++ ansible_install_on_debian_wheezy.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
-
 # Ansible on Debian Wheezy
 
 # Requirements
-apt-get install -y git-core
-apt-get install -y build-essential git-core libssl-dev curl wget
+apt-get install -y build-essential git-core libssl-dev curl wget lsb-release
 
-# facter (2.3.0)
-apt-get install -y lsb-release
+## Facter (2.3.0)
 wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 dpkg -i puppetlabs-release-wheezy.deb
 apt-get update
-apt-get install -y facter=2.3.0-1puppetlabs1_all.deb
+apt-get install -y facter=2.3.0-1puppetlabs1
 
-# Install from repositories (1.7.2)
+# Installation
+
+## Install from repositories (1.7.2)
 #apt-get install -y python-setuptools python-dev python-yaml python-paramiko python-jinja2 sshpass
 #apt-get install -y python-pip
 #apt-get install -y -t wheezy-backports ansible
 
-# Install from source (1.8.0)
+## Install from source (1.8.0)
 git clone --branch v1.8.0 https://github.com/ansible/ansible.git /opt/ansible
 apt-get install -y python-setuptools python-pip python-dev sshpass
 pip install PyYAML jinja2 paramiko
@@ -29,5 +28,5 @@ make install
 export ANSIBLE_HOSTS=./ansible-tools/hosts
 sh -c 'echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config'
 
-# Test
+# Testing
 ansible all --inventory-file=./ansible-tools/hosts -m ping
