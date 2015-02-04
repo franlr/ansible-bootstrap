6aae59ccb3fdb6d43420f3dcf267187bdfc3e9ca
Francisco Lois <franlr@users.noreply.github.com>
2015-02-04 23:19:54 +0100

    Update ansible_install_on_centos_6.sh

--- ansible_install_on_centos_6.sh
+++ ansible_install_on_centos_6.sh
@@ -9,7 +9,18 @@ yum install facter
 yum install -y python-devel python-setuptools
 # yum install -y python-paramiko PyYAML python-jinja2
 yum --enablerepo=epel* install -y ansible sshpass gmp python-pip
+# centos 6.5 msg:
+# [WARNING]: The version of gmp you have installed has a known issue regarding
+# timing vulnerabilities when used with pycrypto. If possible, you should update
+# it (i.e. yum update gmp).
+# https://github.com/ualbertalib/Developer-Handbook/tree/master/Ansible
+yum --enablerepo=epel* install -y gmp-devel
+# recommended: yum install -y openssh-clients
+#
+#
 # or installed from source
+#
+#
 yum groupinstall -y 'Development Tools'
 cd /opt
 git clone git://github.com/ansible/ansible.git
