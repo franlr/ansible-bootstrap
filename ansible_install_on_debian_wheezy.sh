0b8e439c5e0c98b8f45423fd528286a4f4f93d20
Francisco Lois <franlr@users.noreply.github.com>
2015-04-07 14:50:33 +0200

    Update ansible_install_on_debian_wheezy.sh

--- ansible_install_on_debian_wheezy.sh
+++ ansible_install_on_debian_wheezy.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-# Install and configure Ansible on Debian 7 (Wheezy)
+# Install and configure Facter (PupeptLabs) and Ansible on Debian 7 ("Wheezy")
 
-# ensure that this script is run by root
+export CODENAME=`lsb_release -c | awk '{print $2}'`
+export FACTER_VERSION="2.3.0"
+export SUFFIX="1puppetlabs1"
+export TAG="1.9.0"
+export CONFDIR="/etc/ansible"
+filepath=`pwd`
+
+# Ensure that this script is run by root
 if [ $(id -u) -ne 0 ]; then
 sudo $0
 exit
@@ -17,15 +24,9 @@ fi
 
 testmkdir "/root/software"
 
-# Requirements
-apt-get install -y build-essential git-core libssl-dev curl wget lsb-release
-
-export CODENAME=`lsb_release -c | awk '{print $2}'`
-export FACTER_VERSION="2.3.0"
-export SUFFIX="1puppetlabs1"
-export TAG="1.8.2"
-export CONFDIR="/etc/ansible"
-filepath=`pwd`
+# Requirements and utilities
+apt-get install -y build-essential lsb-release
+apt-get install -y git-core libssl-dev curl wget
 
 ## Facter (2.3.0)
 wget -O /root/software/puppetlabs-release-$CODENAME.deb http://apt.puppetlabs.com/puppetlabs-release-$CODENAME.deb
@@ -36,17 +37,20 @@ rm -f /root/software/puppetlabs-release-$CODENAME.deb
 
 # Ansible installation
 
-## Install from repositories (1.7.2)
-#apt-get install -y python-setuptools python-dev python-yaml python-paramiko python-jinja2 sshpass
-#apt-get install -y python-pip
-#apt-get install -y -t wheezy-backports ansible
+### Install from repositories (1.7.2)
+apt-get install -y python-pip python-setuptools python-dev python-dev-all python-yaml \
+# python-paramiko python-jinja2 sshpass
+# apt-get install -y -t wheezy-backports ansible
 
-## Install from source (1.8.2)
-git clone --branch v$TAG https://github.com/ansible/ansible.git /opt/ansible
-apt-get install -y python-setuptools python-pip python-dev sshpass
-pip install PyYAML jinja2 paramiko
+## Install from source (1.9.0)
+git clone --branch v$TAG https://github.com/ansible/ansible.git /opt/ansible --recursive
+# pip install PyYAML jinja2 paramiko
 cd /opt/ansible
-make install
+# source ./hacking/env-setup
+## or
+# make install
+make deb
+dpkg -i deb-build/unstable/ansible*.deb
 
 echo "***************************************************************"
 echo "version facter `facter -v`"
@@ -66,9 +70,6 @@ testmkdir $CONFDIR/filter_plugins
 # http://docs.ansible.com/intro_configuration.html
 
 cp /opt/ansible/examples/ansible.cfg $CONFDIR/
-
-
-#export ANSIBLE_HOST_KEY_CHECKING=False
 sh -c 'echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config'
 sed -i 's/^#remote_port$/remote_port/' /etc/ansible/ansible.cfg
 sed -i 's/^#roles_path$/roles_path/' /etc/ansible/ansible.cfg
@@ -77,8 +78,10 @@ sed -i 's/^#remote_user$/remote_user/' /etc/ansible/ansible.cfg
 sed -i 's/^#scp_if_ssh$/scp_if_ssh/' /etc/ansible/ansible.cfg
 
 # Hosts
-#export ANSIBLE_HOSTS=/etc/ansible/hosts
 cp $filepath/hosts $CONFDIR/
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOSTS=/etc/ansible/hosts
 
 # Testing
 #ansible all --inventory-file=/etc/ansible/hosts -m ping
