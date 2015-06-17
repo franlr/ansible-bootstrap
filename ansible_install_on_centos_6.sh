eb74100fe3b01988a11c71705cd180300798a01e
Francisco Lois <franlr@users.noreply.github.com>
2015-06-17 10:42:45 +0200

    Update ansible_install_on_centos_6.sh

--- ansible_install_on_centos_6.sh
+++ ansible_install_on_centos_6.sh
@@ -4,11 +4,11 @@
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 #
 # From centos repositories or from yum.puppetlabs.com:
-yum install facter
+yum install-y facter
 
 yum install -y python-devel python-setuptools
 # yum install -y python-paramiko PyYAML python-jinja2
-yum --enablerepo=epel* install -y ansible sshpass gmp python-pip
+yum --enablerepo=epel* install -y ansible sshpass gmp python-pip python-six
 # centos 6.5 msg:
 # [WARNING]: The version of gmp you have installed has a known issue regarding
 # timing vulnerabilities when used with pycrypto. If possible, you should update
@@ -24,8 +24,8 @@ yum --enablerepo=epel* install -y gmp-devel asciidoc
 yum groupinstall -y 'Development Tools'
 cd /opt
 git clone git://github.com/ansible/ansible.git
-cd ./ansible
+cd /opt/ansible
 make rpm
-rpm -Uvh ~/rpmbuild/ansible-*.noarch.rpm
+rpm -Uvh ~/rpm-build/ansible-*.noarch.rpm
 
 export ANSIBLE_HOSTS=/etc/ansible/hosts
